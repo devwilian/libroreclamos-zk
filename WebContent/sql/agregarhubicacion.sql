d594f200268fab7190b5f9cc62208551f36df2a8
devwilian <wdarioh95@gmail.com>
2020-12-02 20:08:57 -0500

    sql de agregar columnas de region, provincia, distrito

--- WebContent/sql/agregarhubicacion.sql
+++ WebContent/sql/agregarhubicacion.sql
@@ -1,4 +1,7 @@
+-- AGREGAR LAS COLUMNAS DE PAIS CIUDAD PROVINCIAS, DISTRITO
 ALTER TABLE tsolicitante ADD COLUMN pais varchar(100);
 ALTER TABLE tsolicitante ADD COLUMN ciudad varchar(100);
 ALTER TABLE tsolicitante ADD COLUMN provincia varchar(100);
 ALTER TABLE tsolicitante ADD COLUMN distrito varchar(100);
+
+-- AGREGAR EMPRESAS
